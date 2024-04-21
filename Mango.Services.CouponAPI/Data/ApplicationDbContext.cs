62bb8241232b1fc03a8c7ac8dbf7222eb3e870d1
Timur MIngaziev <tminraz@gmail.com>
2024-04-21 19:36:20 +0200

    Database configuration

---
+++ Mango.Services.CouponAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+using Mango.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.CouponAPI.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Coupon> Coupons { get; set; }
+}
